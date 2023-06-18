d97e02c2c97909a763aa48e8fc16968a91c62e7d
Hongrui Fang <chfanghr@gmail.com>
2023-06-18 23:35:27 +0800

    add `home-manager` binary

--- flake.nix
+++ flake.nix
@@ -3,9 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
@@ -20,6 +28,11 @@
         modules = [
           ./hardware-configuration.nix
           ./configuration.nix
+          {
+            environment.systemPackages = [
+              home-manager.packages."${system}".home-manager
+            ];
+          }
         ];
         specialArgs = {
           inherit hostname;
