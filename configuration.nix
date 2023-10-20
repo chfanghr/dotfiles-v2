9616c60a3e972171d4c2a6de4b0da4fdf0db82a7
Hongrui Fang <chfanghr@gmail.com>
2023-10-20 17:00:43 +0800

    disable iog's cache

--- configuration.nix
+++ configuration.nix
@@ -55,7 +55,6 @@
       substituters = [
         "https://cache.nixos.org/"
         "https://iohk.cachix.org"
-        "https://cache.iog.io"
       ];
       trusted-public-keys = [
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
