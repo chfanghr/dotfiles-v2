d0cbb47fa32c00f7ad48b664da354d671f6d887b
Hongrui Fang <chfanghr@gmail.com>
2024-01-02 14:57:41 +0800

    add mlabs cache to trusted cache

--- configuration.nix
+++ configuration.nix
@@ -58,12 +58,14 @@
       substituters = [
         "https://cache.nixos.org?priority=1"
         "https://nix-community.cachix.org?priority=2"
+        "https://mlabs.cachix.org?priority=3"
         "https://iohk.cachix.org?priority=999"
       ];
       trusted-public-keys = [
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+        "mlabs.cachix.org-1:gStKdEqNKcrlSQw5iMW6wFCj3+b+1ASpBVY2SYuNV2M="
       ];
     };
   };
