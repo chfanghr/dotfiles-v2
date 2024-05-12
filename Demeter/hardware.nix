6a9b53cf3f9e65b89af7e745b2092831aa33f99f
Hongrui Fang <chfanghr@gmail.com>
2024-05-13 00:18:25 +0800

    firmware

--- Demeter/hardware.nix
+++ Demeter/hardware.nix
@@ -5,4 +5,5 @@
 }: {
   nixpkgs.hostPlatform = "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableRedistributableFirmware = true;
 }
