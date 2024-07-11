4e0010135042e4ee8aea4504593279ef3f62b010
Hongrui Fang <chfanghr@gmail.com>
2024-07-11 23:12:54 +0800

    add no chat report mod

--- hosts/Demeter/minecraft.nix
+++ hosts/Demeter/minecraft.nix
@@ -25,6 +25,10 @@
           url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/nMhjKWVE/lithium-fabric-mc1.20.4-0.12.1.jar";
           sha512 = "70bea154eaafb2e4b5cb755cdb12c55d50f9296ab4c2855399da548f72d6d24c0a9f77e3da2b2ea5f47fa91d1258df4d08c6c6f24a25da887ed71cea93502508";
         };
+        no-chat-report = pkgs.fetchurl {
+          url = "https://cdn.modrinth.com/data/qQyHxfxd/versions/Pjto4zdj/NoChatReports-FABRIC-1.20.4-v2.6.1.jar";
+          sha512 = "a2ca389f4024a2089dd1224713e23f356067fbfd27aaf6f3aa74ad28b75d6f0d19e0ed07e721035943964730a2d2a09473067d0d4db34ce7d5b7d6a15a6a5b42";
+        };
       });
     in {
       enable = true;
