8a505b9189f0273b9fba389aaa81c20760f11bc5
Hongrui Fang <chfanghr@gmail.com>
2024-07-14 03:18:20 +0800

    only include certain packages for fanghr when desktop purpose is true

--- modules/home-manager/fanghr/packages.nix
+++ modules/home-manager/fanghr/packages.nix
@@ -1,44 +1,53 @@
 {
   pkgs,
   lib,
+  config,
   ...
-}: {
-  home.packages = with pkgs; [
-    # misc
-    httpie
-    usbutils
-    bat
-    htop
-    netcat
-    coreutils
-    pinentry-curses
-    psmisc
-    jq
-    gnumake
-    treefmt
-    ripgrep
-    wakatime
-    tmux
-    multimarkdown
-    distrobox
-    btop
-    dua
-    spotify
-    moonlight-qt
+}:
+lib.mkMerge [
+  {
+    home.packages = with pkgs; [
+      # misc
+      httpie
+      usbutils
+      bat
+      htop
+      netcat
+      coreutils
+      pinentry-curses
+      psmisc
+      jq
+      gnumake
+      treefmt
+      ripgrep
+      wakatime
+      tmux
+      multimarkdown
+      distrobox
+      btop
+      dua
 
-    # nix
-    nixpkgs-fmt
-    alejandra
-    nil
-    nix-output-monitor
-    nix-prefetch-github
-
-    # fonts
-    nerdfonts
-  ];
-
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "spotify"
+      # nix
+      nixpkgs-fmt
+      alejandra
+      nil
+      nix-output-monitor
+      nix-prefetch-github
     ];
-}
+  }
+  (
+    lib.mkIf config.dotfiles.shared.props.purposes.graphical.desktop {
+      home.packages = with pkgs; [
+        spotify
+        moonlight-qt
+
+        # fonts
+        nerdfonts
+      ];
+      nixpkgs.config.allowUnfreePredicate = pkg:
+        builtins.elem (lib.getName pkg) [
+          "spotify"
+        ];
+    }
+  )
+]
