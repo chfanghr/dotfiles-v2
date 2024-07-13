44019a86c7ffd3c2084fd561dff2d72c36df8c1b
Hongrui Fang <chfanghr@gmail.com>
2024-07-14 05:32:48 +0800

    sambda

--- hosts/Artemis/samba.nix
+++ hosts/Artemis/samba.nix
@@ -1,20 +1,54 @@
 {
-  services.samba.shares = {
-    tank = {
-      path = "/mnt/tank";
-      browseable = "yes";
-      "read only" = "no";
-      "guest ok" = "no";
-      "create mask" = "0644";
-      "directory mask" = "0755";
-      "valid users" = "fanghr";
-      writeable = "yes";
-    };
-    guest = {
-      path = "/mnt/tank/Guest";
-      browseable = "yes";
-      "guest ok" = "yes";
-      writeable = "no";
+  services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
+
+  networking.firewall.allowedTCPPorts = [
+    5357 # wsdd
+  ];
+
+  networking.firewall.allowedUDPPorts = [
+    3702 # wsdd
+  ];
+
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server role = standalone server
+      server min protocol = SMB2_02
+
+      hosts allow = 10.42.0. 127.0.0.1 100.
+      hosts deny = 0.0.0.0/0
+
+      hide unreadable = yes
+
+      guest account = nobody
+      map to guest = bad user
+      usershare allow guests = no
+
+      log file = /var/log/samba/client.%I
+      log level = 2
+
+      create mask = 0664
+      force create mode = 0664
+      directory mask = 0775
+      force directory mode = 0775
+      follow symlinks = yes
+      vfs objects = acl_xattr catia fruit streams_xattr
+      inherit permissions = yes
+    '';
+    shares = {
+      global = {
+        browseable = "yes";
+        "read only" = "yes";
+        "guest ok" = "no";
+      };
+      homes = {
+        browseable = "no";
+        "read only" = "no";
+        "guest ok" = "no";
+      };
     };
   };
 }
--- hosts/Artemis/tank.nix
+++ hosts/Artemis/tank.nix
@@ -6,4 +6,23 @@
     fsType = "btrfs";
     options = ["subvol=tank"];
   };
+
+  services.samba.shares = {
+    tank = {
+      path = "/mnt/tank";
+      browseable = "yes";
+      "read only" = "no";
+      "guest ok" = "no";
+      "create mask" = "0644";
+      "directory mask" = "0755";
+      "valid users" = "fanghr";
+      writeable = "yes";
+    };
+    guest = {
+      path = "/mnt/tank/Guest";
+      browseable = "yes";
+      "guest ok" = "yes";
+      writeable = "no";
+    };
+  };
 }
