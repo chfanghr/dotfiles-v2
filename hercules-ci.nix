39cbb08d509d504df1f9841297521c15d3e13f9a
Hongrui Fang <chfanghr@gmail.com>
2024-10-06 13:05:06 +0800

    schedule regular flake update

--- hercules-ci.nix
+++ hercules-ci.nix
@@ -3,5 +3,17 @@
     inputs.hci-effects.flakeModule
   ];
 
+  hercules-ci.flake-update = {
+    enable = true;
+    baseBranch = "main";
+    createPullRequest = true;
+    autoMergeMethod = "merge";
+    when = {
+      minute = 45;
+      hour = 12;
+      dayOfWeek = "Sun";
+    };
+  };
+
   herculesCI.ciSystems = ["x86_64-linux"];
 }
