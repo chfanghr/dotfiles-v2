50b516001f8c86ee9f384032d27234555c46b3f2
Hongrui Fang <chfanghr@gmail.com>
2024-12-18 18:32:41 +0800

    add fry to oizys

--- hosts/Oizys/management.nix
+++ hosts/Oizys/management.nix
@@ -21,6 +21,17 @@ in {
       home = "/home/fanghr";
       shell = pkgs.zsh;
     };
+    users.fry = {
+      openssh.authorizedKeys.keys = [
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIEMm5O1Fc7pSfuPD5uuZ1I4PmZsP8WzJHiOv7Zqq7RT046ERv7YEC+TMD9X7nAXZX3bM+faPzQPv+XQW3T4L7c= fry@DESKTOP-D7VJM6V"
+      ];
+      isNormalUser = true;
+      extraGroups = ["wheel"];
+      createHome = true;
+      hashedPassword = "$y$j9T$5wDnJ3y8ljamKvg.RxxlN0$dc.j9SXNObdszbooMvqybwNZZDfKbk0gYXF7TrbLDu0";
+      home = "/home/fry";
+      shell = pkgs.zsh;
+    };
     mutableUsers = false;
   };
 
