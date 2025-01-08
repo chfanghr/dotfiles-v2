6e02b8dda8de6efa75b69b983734f3137b732c93
Hongrui Fang <chfanghr@gmail.com>
2025-01-09 01:04:48 +0800

    zsh config

--- modules/home-manager/fanghr/zsh.nix
+++ modules/home-manager/fanghr/zsh.nix
@@ -1,34 +1,65 @@
-{pkgs, ...}: {
+{
   programs = {
     zsh = {
       enable = true;
-      autosuggestion.enable = true;
-      enableCompletion = true;
-      syntaxHighlighting.enable = true;
-      oh-my-zsh = {
-        enable = true;
-        plugins = ["history" "git" "sudo"];
-        theme = "kardan";
+      enableVteIntegration = true;
+
+      history = {
+        size = 1000000;
+        path = "$HOME/.local/share/zsh/history";
       };
+
+      initExtra = ''
+        if [[ -r "$HOME/.iterm2_shell_integration.zsh" ]]; then
+          source "$HOME/.iterm2_shell_integration.zsh"
+        fi
+
+        export GPG_TTY="$(tty)"
+      '';
+
+      prezto = {
+        enable = true;
+        prompt = {
+          theme = "smiley";
+          showReturnVal = true;
+          pwdLength = "short";
+        };
+        terminal.autoTitle = true;
+        syntaxHighlighting.highlighters = [
+          "main"
+          "brackets"
+          "pattern"
+          "line"
+          "root"
+        ];
+        pmodules = [
+          "git"
+          "environment"
+          "terminal"
+          "editor"
+          "history"
+          "directory"
+          "spectrum"
+          "utility"
+          "completion"
+          "prompt"
+          "syntax-highlighting"
+          "history-substring-search"
+          "autosuggestions"
+          "rsync"
+          "ssh"
+          "osx"
+        ];
+      };
+
       shellAliases = {
         ssh-dont-check-host-key = ''ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no"'';
       };
-      plugins = [
-        {
-          name = "zsh-wakatime";
-          src = pkgs.fetchFromGitHub {
-            owner = "wbingli";
-            repo = "zsh-wakatime";
-            rev = "7396e143f2eb048a4a6d64dacae81c52e1ad72ab";
-            sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-          };
-        }
-      ];
     };
 
-    lsd = {
+    eza = {
       enable = true;
-      enableAliases = true;
+      enableZshIntegration = true;
     };
 
     zellij.enable = true;
