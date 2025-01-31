aaf8c17481d5553e0bd6c38d6e56fbd696217792
Hongrui Fang <chfanghr@gmail.com>
2025-01-31 22:20:07 +0800

    update

--- hosts/Eros/hacks.nix
+++ hosts/Eros/hacks.nix
@@ -8,13 +8,6 @@
           Unit = "reboot.target";
         };
       };
-      force-start-pppd-main = {
-        wantedBy = ["timers.target"];
-        timerConfig = {
-          OnUnitInactiveSec = "60";
-          Unit = "pppd-main.service";
-        };
-      };
     };
   };
 }
