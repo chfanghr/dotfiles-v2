4fb675ac87d106dc616ec2ffda415ddf8621c77e
Hongrui Fang <chfanghr@gmail.com>
2025-04-08 21:00:37 +0800

    fix wrong username

--- modules/nixos/common/users/fry.nix
+++ modules/nixos/common/users/fry.nix
@@ -11,7 +11,7 @@ in {
   };
 
   config = mkIf config.dotfiles.nixos.props.users.guests.fry {
-    users.users.robertchen = {
+    users.users.fry = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIEMm5O1Fc7pSfuPD5uuZ1I4PmZsP8WzJHiOv7Zqq7RT046ERv7YEC+TMD9X7nAXZX3bM+faPzQPv+XQW3T4L7c= fry@DESKTOP-D7VJM6V"
