b2ef324e06a3cf5bfccaedb0fe0bad150cfe5d99
Hongrui Fang <chfanghr@gmail.com>
2025-05-04 04:06:35 +0800

    add legacy 24.11 nixpkgs input

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-2411.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
