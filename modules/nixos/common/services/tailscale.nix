af7cbc7aacf37598b1cfb6a098941b4aac5be51b
Hongrui Fang <chfanghr@gmail.com>
2025-05-27 00:24:06 +0800

    downgrade tailscale

--- modules/nixos/common/services/tailscale.nix
+++ modules/nixos/common/services/tailscale.nix
@@ -1,6 +1,16 @@
-{config, ...}: {
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}: let
+  pkgs2411 = import inputs.nixpkgs-2411 {
+    inherit (pkgs.stdenv) system;
+  };
+in {
   services.tailscale = {
     enable = true;
+    package = pkgs2411.tailscale;
     useRoutingFeatures =
       if config.dotfiles.shared.props.purposes.vps
       then "server"
