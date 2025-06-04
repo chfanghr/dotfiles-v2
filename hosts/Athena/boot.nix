46539dc3b10380b7ed28990c1ef82d07b7b4c95b
Hongrui Fang <chfanghr@gmail.com>
2025-06-04 18:13:04 +0800

    actually rollback root filesystem on athena

--- hosts/Athena/boot.nix
+++ hosts/Athena/boot.nix
@@ -1,13 +1,32 @@
-{
+{config, ...}: {
   boot = {
-    initrd.availableKernelModules = [
-      "xhci_pci"
-      "ahci"
-      "usbhid"
-      "usb_storage"
-      "sd_mod"
-      "igc"
-    ];
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+        "igc"
+      ];
+
+      systemd = {
+        enable = true;
+
+        services.rollback = {
+          description = "Rollback root filesystem to a pristine state";
+          wantedBy = ["initrd.target"];
+          after = ["zfs-import-rpool.service"];
+          before = ["sysroot.mount"];
+          path = [config.boot.zfs.package];
+          unitConfig.DefaultDependencies = "no";
+          serviceConfig.Type = "oneshot";
+          script = ''
+            zfs rollback -r rpool/root@blank && echo " >> >> Rollback Complete << <<"
+          '';
+        };
+      };
+    };
 
     loader = {
       systemd-boot.enable = true;
