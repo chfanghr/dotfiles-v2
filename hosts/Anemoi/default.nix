c30c6b4afb7d6412258a2557b9e37e2f333afd3f
Hongrui Fang <chfanghr@gmail.com>
2025-10-26 17:48:36 +0800

    add sbctl

--- hosts/Anemoi/default.nix
+++ hosts/Anemoi/default.nix
@@ -1,10 +1,15 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     ./boot.nix
     ./disko-config.nix
     ./networking.nix
     ../../modules/nixos/common
     inputs.disko.nixosModules.default
+    inputs.lanzaboote.nixosModules.lanzaboote
   ];
 
   dotfiles.nixos = {
@@ -29,4 +34,8 @@
   users.users.fanghr.hashedPassword = "$y$j9T$h8nXdACDqyTyEO1AXMHvn/$YPSBkrTjrUmdk2DY6qH5TDepxd7yPm2hcPngIk9lVJD";
 
   nix.settings.download-buffer-size = 1048576000;
+
+  environment.defaultPackages = [
+    pkgs.sbctl
+  ];
 }
