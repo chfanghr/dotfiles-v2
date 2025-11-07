731b58dd213a9d092fd560e67757c526a7548f03
Hongrui Fang <chfanghr@gmail.com>
2025-11-07 19:21:22 +0800

    enable jj

--- modules/home-manager/fanghr/default.nix
+++ modules/home-manager/fanghr/default.nix
@@ -6,6 +6,7 @@
     ./fonts.nix
     ./git.nix
     ./gpg.nix
+    ./jj.nix
     ./mangohud.nix
     ./hyprland.nix
     ./nvim.nix
---
+++ modules/home-manager/fanghr/jj.nix
@@ -0,0 +1,11 @@
+{
+  programs.jujutsu = {
+    enable = true;
+    ediff = false;
+
+    settings = {
+      name = "Hongrui Fang";
+      email = "chfanghr@gmail.com";
+    };
+  };
+}
