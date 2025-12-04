b171dbccb98e0168933cc9ab74b0c94eb31ba853
Hongrui Fang <chfanghr@gmail.com>
2025-12-04 22:26:40 +0800

    resolve some warnings

--- modules/home-manager/fanghr/git.nix
+++ modules/home-manager/fanghr/git.nix
@@ -1,8 +1,8 @@
 {
+  programs.difftastic.enable = false;
+
   programs.git = {
     enable = true;
-
-    difftastic.enable = false;
 
     ignores = [
       "*~"
@@ -15,24 +15,28 @@
       "clear\\ /"
     ];
 
-    userEmail = "chfanghr@gmail.com";
-    userName = "Hongrui Fang";
+    settings = {
+      user = {
+        email = "chfanghr@gmail.com";
+        name = "Hongrui Fang";
+      };
 
-    aliases = rec {
-      lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
-      lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
-      lg = lg1;
-      hh = "rev-parse HEAD";
-    };
+      aliases = rec {
+        lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+        lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+        lg = lg1;
+        hh = "rev-parse HEAD";
+      };
 
-    extraConfig = {
-      core.autocrlf = "input";
-      core.editor = "nvim";
-      init.defaultBranch = "main";
-      "credential \"https://gitlab.com\"" = {
-        username = "connor81";
-        helper = "store";
-        useHttpPath = true;
+      extraConfig = {
+        core.autocrlf = "input";
+        core.editor = "nvim";
+        init.defaultBranch = "main";
+        "credential \"https://gitlab.com\"" = {
+          username = "connor81";
+          helper = "store";
+          useHttpPath = true;
+        };
       };
     };
 
--- modules/nixos/common/desktop/greetd.nix
+++ modules/nixos/common/desktop/greetd.nix
@@ -10,34 +10,15 @@ in
     && !config.dotfiles.shared.props.hardware.steamdeck) {
     services.greetd = {
       enable = true;
+      useTextGreeter = true;
       settings = {
         default_session.command = ''
-          ${pkgs.greetd.tuigreet}/bin/tuigreet \
+          ${pkgs.tuigreet}/bin/tuigreet \
             --time \
             --asterisks \
-            --user-menu \
-            --cmd hyprland
+            --user-menu
         '';
       };
-    };
-
-    environment.etc."greetd/environments".text = ''
-      hyprland
-    '';
-
-    # this is a life saver.
-    # literally no documentation about this anywhere.
-    # might be good to write about this...
-    # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
-    systemd.services.greetd.serviceConfig = {
-      Type = "idle";
-      StandardInput = "tty";
-      StandardOutput = "tty";
-      StandardError = "journal"; # Without this errors will spam on screen
-      # Without these bootlogs will spam on screen
-      TTYReset = true;
-      TTYVHangup = true;
-      TTYVTDisallocate = true;
     };
 
     boot.plymouth.enable = mkDefault true;
