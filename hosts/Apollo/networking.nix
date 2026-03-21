8800b7ed46e1bb746d5930c4ddf1e3cd8bf58ce4
Hongrui Fang <chfanghr@gmail.com>
2026-03-22 02:55:39 +0800

    configure vlan on apollo

--- hosts/Apollo/networking.nix
+++ hosts/Apollo/networking.nix
@@ -3,6 +3,27 @@
     useNetworkd = true;
     firewall.enable = true;
     nftables.enable = true;
-    interfaces.enp15s0.useDHCP = true;
+    vlans.vlan-main = {
+      id = 42;
+      interface = "enp13s0f0np0";
+    };
+    interfaces = {
+      enp15s0.useDHCP = true;
+      enp13s0f0np0.useDHCP = false;
+      enp13s0f0np1.useDHCP = true;
+      vlan-main.useDHCP = true;
+    };
+    enableIPv6 = true;
+  };
+
+  systemd.network.networks = {
+    "40-enp13s0f0np0".networkConfig = {
+      LLDP = true;
+      EmitLLDP = true;
+      IPv6AcceptRA = false;
+    };
+    "40-vlan-main".networkConfig = {
+      IPv6AcceptRA = true;
+    };
   };
 }
