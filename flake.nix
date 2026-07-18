315e95cd8830716a3245a64e842f6a395049fe74
Hongrui Fang <chfanghr@gmail.com>
2026-07-19 03:34:59 +0800

    introduce nix-index-database

--- flake.nix
+++ flake.nix
@@ -58,6 +58,10 @@
       url = "github:OJII3/dotfiles";
       flake = false;
     };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {flake-parts, ...}:
