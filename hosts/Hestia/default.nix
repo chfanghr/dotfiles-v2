168dfeae1741d8f010ecc31c3a52048fb943b11b
Hongrui Fang <chfanghr@gmail.com>
2026-07-19 03:39:47 +0800

    hestia: enable nix-index-database

--- hosts/Hestia/default.nix
+++ hosts/Hestia/default.nix
@@ -23,6 +23,7 @@ in {
     ../../modules/nixos/common
     inputs.disko.nixosModules.default
     inputs.lanzaboote.nixosModules.lanzaboote
+    inputs.nix-index-database.nixosModules.default
   ];
 
   dotfiles.nixos = {
