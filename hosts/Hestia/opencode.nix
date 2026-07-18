a326849f8584a7f2d811d74f52ed8bb04319eef0
Hongrui Fang <chfanghr@gmail.com>
2026-07-19 03:20:23 +0800

    hestia: install nix skills

--- hosts/Hestia/opencode.nix
+++ hosts/Hestia/opencode.nix
@@ -4,12 +4,16 @@
   ...
 }: {
   home-manager.users.fanghr = {
+    imports = [inputs.agent-skills.homeManagerModules.default];
+
     programs = {
       opencode = {
         enable = true;
         enableMcpIntegration = true;
         extraPackages = [
-          pkgs.python3
+          (pkgs.python3.withPackages (pyPkgs: [
+            pyPkgs.pypdf
+          ]))
         ];
         settings = {
           autoupdate = false;
@@ -29,6 +33,36 @@
         servers = {
           nixos.command = "${inputs.mcp-nixos.packages.${pkgs.stdenv.system}.default}/bin/mcp-nixos";
         };
+      };
+
+      agent-skills = {
+        enable = true;
+        targets.opencode.enable = true;
+        sources = {
+          anthropic-skills = {
+            path = inputs.anthropic-skills;
+            subdir = "skills";
+          };
+          nhooey-nix-skills = {
+            path = inputs.nhooey-nix-skills;
+            subdir = "skills";
+          };
+          ojii3-dotfiles = {
+            path = inputs.ojii3-dotfiles;
+            subdir = "modules/home/ai/skills";
+          };
+        };
+        skills.enable = [
+          # anthropic-skills
+          "pdf"
+          # nhooey-nix-skills
+          "nix-flakes"
+          "nix-flake-recursive-bump-input-versions"
+          # ojii3-dotfiles
+          "using-flake-parts"
+          "missing-tools"
+          "nix-module-options"
+        ];
       };
     };
   };
