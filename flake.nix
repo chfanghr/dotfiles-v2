9d52d60df8e9327e366ec28904e023a308a50ae7
Hongrui Fang <chfanghr@gmail.com>
2026-09-19 19:25:47 +0800

    introduce nixpkgs-multiverse

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs-multiverse.url = "github:fzakaria/nixpkgs-multiverse";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
