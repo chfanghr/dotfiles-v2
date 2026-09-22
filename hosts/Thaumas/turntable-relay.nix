9e39d889d96a2e579920048803bfc32a76f5cb91
Hongrui Fang <chfanghr@gmail.com>
2026-09-22 19:51:47 +0800

    thaumas: remove dead code

--- hosts/Thaumas/turntable-relay.nix
+++ hosts/Thaumas/turntable-relay.nix
@@ -4,18 +4,12 @@
   ...
 }: {
   services.pipewire.systemWide = true;
-  systemd.user.services.wireplumber.wantedBy = ["default.target"];
-  users.users.fanghr = {
-    linger = true;
-    extraGroups = ["audio" "pipewire" config.hardware.i2c.group];
-  };
 
-  hardware = {
-    i2c.enable = true;
-    raspberry-pi."4" = {
-      i2c1.enable = true;
-      gpio.enable = true;
-    };
+  users.users.fanghr.extraGroups = ["audio" "pipewire" config.hardware.i2c.group];
+
+  hardware.raspberry-pi."4" = {
+    i2c1.enable = true;
+    gpio.enable = true;
   };
 
   environment.defaultPackages = [
